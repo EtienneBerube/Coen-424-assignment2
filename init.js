5ebead3ee0631b83a2fcd45473c03f20b351e4a9
Etienne <tiennebrb@gmail.com>
2019-12-11 15:25:53 -0500

    working ish

--- init.js
+++ init.js
@@ -4,25 +4,25 @@ var path = require('path');
 var assert = require('assert');
 var db;
 
-async function initDB(){
+async function initDB() {
     console.log('Connecting...');
-    client = await MongoClient.connect("mongodb://localhost:27017/ass2",{useUnifiedTopology: true});
+    client = await MongoClient.connect("mongodb://localhost:27017/ass2", { useUnifiedTopology: true });
     console.log('Connected');
     db = client.db('ass2');
-    return db; 
+    return db;
 }
 
 
-function sendToDb(object, collection){
+function sendToDb(object, collection) {
     db.collection(collection).insertOne(object);
 }
 
-function parseLine(line, collection){
-    
+function parseLine(line, collection) {
+
     const values = line.split(',');
-    if(values[0] === 'CPUUtilization_Average'){
+    if (values[0] === 'CPUUtilization_Average') {
         return; //First line
-    }else{
+    } else {
         const obj = {
             'CPUUtilization_Average': parseFloat(values[0]),
             'NetworkIn_Average': parseFloat(values[1]),
@@ -31,26 +31,31 @@ function parseLine(line, collection){
             'Final_Target': parseFloat(values[4])
         };
 
-        sendToDb(obj,collection );
+        sendToDb(obj, collection);
     }
 }
 
 initDB().then((db) => {
     console.log('Starts reading...');
-    lineReader.eachLine('resources/DVD-testing.csv', { encoding: 'utf8'}, (line) => {
+    lineReader.eachLine('resources/DVD-testing.csv', { encoding: 'utf8' }, (line) => {
         parseLine(line, 'DVD-testing');
     });
-    
-    lineReader.eachLine('resources/DVD-training.csv', {encoding: 'utf8'},  (line) => {
+
+    lineReader.eachLine('resources/DVD-training.csv', { encoding: 'utf8' }, (line) => {
         parseLine(line, 'DVD-training');
     });
-    
-    lineReader.eachLine('resources/NDBench-testing.csv', {encoding: 'utf8'},  (line) => {
+
+    lineReader.eachLine('resources/NDBench-testing.csv', { encoding: 'utf8' }, (line) => {
         parseLine(line, 'NDBench-testing');
     });
-    
-    lineReader.eachLine('resources/NDBench-training.csv', {encoding: 'utf8'},  (line) => {
+
+    lineReader.eachLine('resources/NDBench-training.csv', { encoding: 'utf8' }, (line) => {
         parseLine(line, 'NDBench-training');
     });
+
+    setTimeout(() => { 
+        console.log('Probably done...');
+         process.exit(0); 
+    }, 30000);
 });
 
--- run.js
+++ run.js
@@ -1,43 +1,41 @@
-var readline = require('readline-sync');
-var MongoClient = require('mongodb').MongoClient;
+var readline = require("readline-sync");
+var MongoClient = require("mongodb").MongoClient;
 
+var mapperCPU = function () {
+    emit("CPUUtilization_Average", this.CPUUtilization_Average);
+};
 
-var mapperCPU = function (){
-    emit('CPUUtilization_Average', this.CPUUtilization_Average);
-}
+var mapperNetworkIn = function () {
+    emit("NetworkIn_Average", this.NetworkIn_Average);
+};
 
-var mapperNetworkIn = function(){
-    emit('NetworkIn_Average', this.NetworkIn_Average);
-}
+var mapperNetworkOut = function () {
+    emit("NetworkOut_Average", this.NetworkOut_Average);
+};
 
-var mapperNetworkOut = function(){
-    emit('NetworkOut_Average', this.NetworkOut_Average);
-}
+var mapperMemory = function () {
+    emit("MemoryUtilization_Average", this.MemoryUtilization_Average);
+};
 
-var mapperMemory = function(){
-    emit('MemoryUtilization_Average', this.MemoryUtilization_Average);
-}
-
-var mapperFinal = function(){
-    emit('Final_Target', this.Final_Target);
-}
+var mapperFinal = function () {
+    emit("Final_Target", this.Final_Target);
+};
 
 var reducerMin = function (key, values) {
     values = Array.sort(values);
     return values[0];
-}
+};
 
 var reducerMax = function (key, values) {
     values = Array.sort(values);
     return values.pop();
-}
+};
 
 var reducerMedian = function (key, values) {
     values = Array.sort(values);
     const index = (values.length - 1) / 2;
     return values[index | 0];
-
-}
+};
 
 var reducerStdDev = function (key, values) {
     const reducer = (accumulator, value) => accumulator + value;
@@ -45,35 +43,51 @@ var reducerStdDev = function (key, values) {
 
     const average = sum / values.length;
 
-    const reducerVariance = ((accumulator, value) => accumulator + Math.pow((value - average), 2));
+    const reducerVariance = (accumulator, value) =>
+        accumulator + Math.pow(value - average, 2);
 
     let variance = values.reduce(reducerVariance, 0);
 
     return Math.sqrt(variance / values.length);
-}
+};
 
 var reducerNorm = function (key, values) {
     values = Array.sort(values);
     const min = values[0];
-    const max = values.pop();
+    let max = values[values.length - 1];
+    let prev_val;
 
-    const normalize = values.map((value) => (value - min) / (max - min));
+    if(typeof max === 'object'){
+        print(max.toSource());
+        prev_val = max.normalized_value;
+        max = values[values.length - 2];
+        values.pop();
+    }
 
-    return normalize;
-}
+    print('before:' + values.join(','));
+    print('min:' + min);
+    print('max:' + max);
+
+    const normalize = values.map((value) => 
+    {
+        let norm_val =  (parseFloat(value) - parseFloat(min)) / (parseFloat(max) - parseFloat(min));
+        print('in Loop: ' + norm_val);
+        return norm_val;
+    });
+
+    print('after loop: '+ values.join(','));
+    return {"normalized_values" : normalize.concat(prev_val)};
+};
 
 var reducer90 = function (key, values) {
     values = Array.sort(values);
 
-    return values[parseInt(0.9 * (values.length))];
-}
+    return values[parseInt(0.9 * values.length)];
+};
 
+run().then(()=>{});
 
-run().then((data) => {
-    console.log(data);
-})
-
-async function run(){
+async function run() {
     let field = getField();
     let action = getAction();
 
@@ -82,111 +96,125 @@ async function run(){
 
     let collectionName = getCollection();
 
-    console.log('Connecting...');
-    client = await MongoClient.connect("mongodb://localhost:27017/ass2",{useUnifiedTopology: true});
-    console.log('Connected');
-    let collection = client.db('ass2').collection(collectionName);
+    console.log("Connecting...");
+    client = await MongoClient.connect("mongodb://localhost:27017/ass2", {
+        useUnifiedTopology: true
+    });
+    console.log("Connected");
+    let collection = client.db("ass2").collection(collectionName);
 
-    
+    collection.mapReduce(
+        map,
+        reduce,
+        { out: { inline: 1 }, verbose: true },
+        function (err, result, stats) {
+            if(err){
+                console.log("Error: ");
+                console.log(err);
+                process.exit(1); 
+            }
+            console.dir(result.results, {depth: null, colors: true, maxArrayLength: null});
+            console.dir(result.stats, {depth: null, colors: true, maxArrayLength: null});
 
-    collection.mapReduce(map, reduce, {out : {inline: 1}, verbose:true}, function(err, results, stats) {
-        console.log(results)
-        console.log(stats)
+            client.close();
 
-        client.close();
-
-        console.log('Done')
-        process.exit(0); 
-      });
+            console.log("Done");
+            process.exit(0);
+        }
+    );
 }
 
 function getField() {
-    let field = readline.question("Which field to analyze: "
-    +"\n1)CPU Utilization Average" 
-    +"\n2)Network In Average" 
-    +"\n3)Network Out Average"
-    +"\n4)Memory Utilization Average"
-    +"\n5)Final_Target"
-    +"\nEnter: ");
+    let field = readline.question(
+        "Which field to analyze: " +
+        "\n1)CPU Utilization Average" +
+        "\n2)Network In Average" +
+        "\n3)Network Out Average" +
+        "\n4)Memory Utilization Average" +
+        "\n5)Final_Target" +
+        "\nEnter: "
+    );
 
-    if(parseInt(field) < 1 || parseInt(field) > 5){
-        console.log('Wrong selection')
-        process.exit(1);  
+    if (parseInt(field) < 1 || parseInt(field) > 5) {
+        console.log("Wrong selection");
+        process.exit(1);
     }
 
     field = parseInt(field);
 
-    return field
-
+    return field;
 }
 
 function getAction() {
-    let action = readline.question("Which action to perform: "
-    +"\n1)Min" 
-    +"\n2)Max" 
-    +"\n3)Median"
-    +"\n4)Standard Deviation"
-    +"\n5)Normalization"
-    + "\n6)90-th percentile"
-    + "\nEnter: ");
+    let action = readline.question(
+        "Which action to perform: " +
+        "\n1)Min" +
+        "\n2)Max" +
+        "\n3)Median" +
+        "\n4)Standard Deviation" +
+        "\n5)Normalization" +
+        "\n6)90-th percentile" +
+        "\nEnter: "
+    );
 
-    if(parseInt(action) < 1 || parseInt(action) > 6){
-        console.log('Wrong selection')
-        process.exit(1);  
+    if (parseInt(action) < 1 || parseInt(action) > 6) {
+        console.log("Wrong selection");
+        process.exit(1);
     }
 
     action = parseInt(action);
 
-    return action
+    return action;
 }
 
 function getCollection() {
-    let collection = readline.question("Which collection to use: "
-    +"\n1)DVD-testing" 
-    +"\n2)DVD-training" 
-    +"\n3)NDBench-testing"
-        + "\n4)NDBench-training"
-        + "\nEnter: ");
+    let collection = readline.question(
+        "Which collection to use: " +
+        "\n1)DVD-testing" +
+        "\n2)DVD-training" +
+        "\n3)NDBench-testing" +
+        "\n4)NDBench-training" +
+        "\nEnter: "
+    );
 
-    if(parseInt(collection) < 1 || parseInt(collection) > 6){
-        console.log('Wrong selection')
-        process.exit(1);  
+    if (parseInt(collection) < 1 || parseInt(collection) > 6) {
+        console.log("Wrong selection");
+        process.exit(1);
     }
 
     collection = parseInt(collection);
 
-    if(collection == 1){
-        return 'DVD-testing';
-    }else if(collection == 2){
-        return 'DVD-training';
-    }else if(collection == 3){
-        return 'NDBench-testing';
-    }else if(collection == 4){
-        return 'NDBench-training';
-    }else{
-        console.log('Wrong selection')
-        process.exit(1);    
+    if (collection == 1) {
+        return "DVD-testing";
+    } else if (collection == 2) {
+        return "DVD-training";
+    } else if (collection == 3) {
+        return "NDBench-testing";
+    } else if (collection == 4) {
+        return "NDBench-training";
+    } else {
+        console.log("Wrong selection");
+        process.exit(1);
     }
 
-    return collection
+    return collection;
 }
 
-function getMapper(field){
-    if(field == 1){
+function getMapper(field) {
+    if (field == 1) {
         return mapperCPU;
-    }else if(field == 2){
+    } else if (field == 2) {
         return mapperNetworkIn;
-    }else if(field == 3){
+    } else if (field == 3) {
         return mapperNetworkOut;
-    }else if(field == 4){
+    } else if (field == 4) {
         return mapperMemory;
-    }else if(field == 5){
+    } else if (field == 5) {
         return mapperFinal;
     }
 }
 
-function getReducer(action){
-
+function getReducer(action) {
     if (action == 1) {
         return reducerMin;
     } else if (action == 2) {
@@ -201,4 +229,3 @@ function getReducer(action){
         return reducer90;
     }
 }
-
